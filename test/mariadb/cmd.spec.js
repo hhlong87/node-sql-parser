2f81d1c70dee9aff34450f19c92a209199fec7e0
Long Hoang <hh.long.87@gmail.com>
2024-03-06 11:57:41 +0700

    support mysql BOOL, postgres alter column

--- test/mariadb/cmd.spec.js
+++ test/mariadb/cmd.spec.js
@@ -23,4 +23,25 @@ describe('MariaDB Command SQL', () => {
       })
     });
   })
+
+  const SQL_LIST = [
+    {
+      title: 'BOOL date type',
+      sql: [
+        "CREATE TABLE `table_name` (`is_bool` bool) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci",
+        "CREATE TABLE `table_name` (`is_bool` BOOL) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci",
+      ],
+    },
+  ]
+
+  function neatlyNestTestedSQL(sqlList) {
+    sqlList.forEach(sqlInfo => {
+      const {title, sql} = sqlInfo
+      it(`should support ${title}`, () => {
+        expect(getParsedSql(sql[0], opt)).to.equal(sql[1])
+      })
+    })
+  }
+
+  neatlyNestTestedSQL(SQL_LIST)
 })
