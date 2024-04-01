70433187660e184a6ed9a081f691f3baabb6db64
Long Hoang <long.hoang@wtc.woowahan.com>
2024-04-01 13:38:52 +0700

    feat: add DOUBLE PRECISION keyword

--- test/mysql/cmd.spec.js
+++ test/mysql/cmd.spec.js
@@ -20,6 +20,20 @@ describe('Mysql Customize', () => {
         "CREATE TABLE `table_name` (`is_bool` BOOL) ENGINE = INNODB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci",
       ],
     },
+    {
+      title: 'Double Precision date type',
+      sql: [
+        "CREATE TABLE `table_name` (`double_precision_val` DOUBLE PRECISION)",
+        "CREATE TABLE `table_name` (`double_precision_val` DOUBLE PRECISION)",
+      ],
+    },
+    {
+      title: 'Double date type',
+      sql: [
+        "CREATE TABLE `table_name` (`double_val` DOUBLE)",
+        "CREATE TABLE `table_name` (`double_val` DOUBLE)",
+      ],
+    },
   ]
 
   function neatlyNestTestedSQL(sqlList) {
